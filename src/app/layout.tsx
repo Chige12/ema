5ce669cb6338875bd2af8cf5f1e5eb09b319de3d
chige12 <tige1215.technic@gmail.com>
2024-12-30 09:44:19 +0900

    ✨ update obp url 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 
@@ -13,20 +14,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <head prefix="og: https://ogp.me/ns#">
+      <Head>
         <meta property="og:url" content="https://emaema.netlify.app/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content='デジタル絵馬を作ろう "ema"' />
         <meta
           property="og:description"
-          content="-願いをもっと簡単に、もっと身近に。いつもは宣言するのが恥ずかしいと思うような願いや夢。でも、大事な願いも秘めていては、忘れてしまう。だから今年こそは、自分に向き合って共有して、絶対に叶える1年にしませんか？ 2024.11.29 リリース"
+          content="願いをもっと簡単に、もっと身近に。いつもは宣言するのが恥ずかしいと思うような願いや夢。でも、大事な願いも秘めていては、忘れてしまう。だから今年こそは、自分に向き合って共有して、絶対に叶える1年にしませんか？ 2024.11.29 リリース"
         />
         <meta property="og:site_name" content='デジタル絵馬を作ろう "ema"' />
         <meta
           property="og:image"
           content="https://emaema.netlify.app/images/obp.png"
         />
-      </head>
+        <title>デジタル絵馬を作ろう &quot;ema&quot;</title>
+        <meta
+          name="description"
+          content="願いをもっと簡単に、もっと身近に。いつもは宣言するのが恥ずかしいと思うような願いや夢。でも、大事な願いも秘めていては、忘れてしまう。だから今年こそは、自分に向き合って共有して、絶対に叶える1年にしませんか？ 2024.11.29 リリース"
+        />
+      </Head>
       <body className="bg-white">{children}</body>
     </html>
   );
