da4a8043c425646a8a243ad284e5e62507f389f0
chige12 <tige1215.technic@gmail.com>
2024-12-30 10:08:45 +0900

    ✨ update obp url 6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,24 @@ import type { Metadata } from 'next';
 import '@/styles/globals.css';
 
 const TITLE = 'デジタル絵馬を作ろう "ema"';
-const DESCRIPTION = '願いをもっと簡単に、もっと身近に。いつもは宣言するのが恥ずかしいと思うような願いや夢。でも、大事な願いも秘めていては、忘れてしまう。だから今年こそは、自分に向き合って共有して、絶対に叶える1年にしませんか？ 2024.11.29 リリース';
+const DESCRIPTION =
+  '願いをもっと簡単に、もっと身近に。いつもは宣言するのが恥ずかしいと思うような願いや夢。でも、大事な願いも秘めていては、忘れてしまう。だから今年こそは、自分に向き合って共有して、絶対に叶える1年にしませんか？ 2024.11.29 リリース';
 
 export const metadata: Metadata = {
   title: TITLE,
   description: DESCRIPTION,
   openGraph: {
-    type: "website",
-    url: "https://emaema.netlify.app/",
+    type: 'website',
+    url: 'https://emaema.netlify.app/',
     title: TITLE,
     description: DESCRIPTION,
     siteName: TITLE,
-    images: [{
-      url: "./images/ogb.png",
-    }],
-  }
+    images: [
+      {
+        url: './images/ogb.png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
