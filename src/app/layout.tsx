1dd85e4cd4a5db22a023ca4817b7e25ae44d3ac1
chige12 <tige1215.technic@gmail.com>
2024-12-30 10:07:55 +0900

    ✨ update obp url 5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,22 @@
-import Head from 'next/head';
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
 
+const TITLE = 'デジタル絵馬を作ろう "ema"';
+const DESCRIPTION = '願いをもっと簡単に、もっと身近に。いつもは宣言するのが恥ずかしいと思うような願いや夢。でも、大事な願いも秘めていては、忘れてしまう。だから今年こそは、自分に向き合って共有して、絶対に叶える1年にしませんか？ 2024.11.29 リリース';
+
 export const metadata: Metadata = {
-  title: '絵馬 2024',
-  description: '絵馬を書いて、願いをかなえよう',
+  title: TITLE,
+  description: DESCRIPTION,
+  openGraph: {
+    type: "website",
+    url: "https://emaema.netlify.app/",
+    title: TITLE,
+    description: DESCRIPTION,
+    siteName: TITLE,
+    images: [{
+      url: "./images/ogb.png",
+    }],
+  }
 };
 
 export default function RootLayout({
@@ -14,25 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <Head>
-        <meta property="og:url" content="https://emaema.netlify.app/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content='デジタル絵馬を作ろう "ema"' />
-        <meta
-          property="og:description"
-          content="願いをもっと簡単に、もっと身近に。いつもは宣言するのが恥ずかしいと思うような願いや夢。でも、大事な願いも秘めていては、忘れてしまう。だから今年こそは、自分に向き合って共有して、絶対に叶える1年にしませんか？ 2024.11.29 リリース"
-        />
-        <meta property="og:site_name" content='デジタル絵馬を作ろう "ema"' />
-        <meta
-          property="og:image"
-          content="https://emaema.netlify.app/images/ogp.png"
-        />
-        <title>デジタル絵馬を作ろう &quot;ema&quot;</title>
-        <meta
-          name="description"
-          content="願いをもっと簡単に、もっと身近に。いつもは宣言するのが恥ずかしいと思うような願いや夢。でも、大事な願いも秘めていては、忘れてしまう。だから今年こそは、自分に向き合って共有して、絶対に叶える1年にしませんか？ 2024.11.29 リリース"
-        />
-      </Head>
       <body className="bg-white">{children}</body>
     </html>
   );
